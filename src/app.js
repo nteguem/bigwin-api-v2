67011d4d2751393d24ff9e9507803b812ced590f
nteguem roland <nteguemroland@gmail.com>
2025-12-01 08:12:51 +0100

    review

--- src/app.js
+++ src/app.js
@@ -17,6 +17,11 @@ const app = express();
 // Middleware essentiels
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  console.log('[DEBUG] req.query:', req.query);
+  console.log('[DEBUG] req.url:', req.url);
+  next();
+});
 app.use(compression());
 app.use(helmet());
 app.use(cors());
