fae41a0be6edf06b3f6b2cc0da392a81d90616cc
nteguem roland <nteguemroland@gmail.com>
2025-12-01 09:11:03 +0100

    review

--- src/api/middlewares/common/appIdentifier.js
+++ src/api/middlewares/common/appIdentifier.js
@@ -1,42 +1,29 @@
 // src/api/middlewares/common/appIdentifier.js
 
-const App = require('../../models/common/App');
-const { AppError, ErrorCodes } = require('../../../utils/AppError');
-
 const identifyApp = async (req, res, next) => {
   try {
-    console.log('[identifyApp] LIGNE 8 - req.query:', req.query);
-    
     const appId = req.headers['x-app-id'];
-    
-    console.log('[identifyApp] LIGNE 12 - req.query:', req.query);
     
     if (!appId) {
       return next(new AppError('Header X-App-Id requis', 400, ErrorCodes.VALIDATION_ERROR));
     }
-    
-    console.log('[identifyApp] LIGNE 18 - req.query:', req.query);
     
     const app = await App.findOne({ 
       appId: appId.toLowerCase(),
       isActive: true 
     }).lean();
     
-    console.log('[identifyApp] LIGNE 25 - req.query:', req.query);
-    
     if (!app) {
       return next(new AppError(`Application '${appId}' non trouvée ou désactivée`, 404, ErrorCodes.VALIDATION_ERROR));
     }
     
-    console.log('[identifyApp] LIGNE 31 - req.query:', req.query);
-    
     req.appId = app.appId;
     
-    console.log('[identifyApp] LIGNE 35 - req.query:', req.query);
+    // ⭐ SOLUTION : NE PAS ATTACHER L'OBJET COMPLET
+    // req.app = app;  // ❌ RETIRE CETTE LIGNE
     
-    req.app = app;
-    
-    console.log('[identifyApp] LIGNE 39 - req.query:', req.query);
+    // ✅ OU attache une copie propre
+    req.app = { ...app };
     
     next();
   } catch (error) {
@@ -61,7 +48,7 @@ const identifyAppOptional = async (req, res, next) => {
     
     if (app) {
       req.appId = app.appId;
-      req.app = app;
+      req.app = { ...app }; // ✅ COPIE
     } else {
       req.appId = null;
       req.app = null;
