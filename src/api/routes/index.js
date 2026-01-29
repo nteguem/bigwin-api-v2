db795c9eacc86d1f4b147cdcde1c63b7f549b001
nteguem roland <nteguemroland@gmail.com>
2026-01-29 09:22:07 +0100

    review

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -98,8 +98,7 @@ router.use('/payments/smobilpay', identifyAppOptional, smobilpayRoutes);
 router.use('/payments/cinetpay', identifyAppOptional, cinetpayRoutes);
 router.use('/payments/afribapay', identifyAppOptional, afribaPayRoutes);
 router.use('/payments/flutterwave', identifyAppOptional, flutterwaveRoutes);
-router.use('/payments/korapay', identifyAppOptional, korapayRoutes); // ⭐ NOUVEAU KORAPAY
-
+router.use('/payments/korapay', korapayRoutes);
 /**
  * GET /api/
  * Documentation des endpoints disponibles
